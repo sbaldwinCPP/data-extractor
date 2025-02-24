5a4e1937adc269d55441be2f695b6f65f07027ab
sbaldwinCPP <sbaldwin@cppwind.com>
2025-02-24 01:21:14 -0700

    basic src files

---
+++ src/core.py
@@ -0,0 +1,63 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+
+# browser popup functions
+def file_browse(multiple=False, filetypes=["*.png", "*.jpg"]):
+    # # filetypes.append(("All Files", "*"))
+    # file = eg.fileopenbox(
+    #     msg="select an image file",
+    #     default="*.png",
+    #     filetypes=filetypes,
+    #     multiple=False,
+    # )
+
+    # return file
+
+    filetypes.append(("All Files", "*"))
+    root = tk.Tk()
+    root.withdraw()
+    root.iconbitmap(AssetManager.file_icon)
+
+    if multiple:
+        path = filedialog.askopenfilenames(filetypes=filetypes)
+    else:
+        path = filedialog.askopenfilename(filetypes=filetypes)
+    root.destroy()
+    return path if path != "" else None
+
+
+class AssetManager:
+    """
+    Stores paths to asset files as attributes.
+    Use in higher level modules to 'ask' for the path to each resource.
+        ex: resource_path = AssetManager.resource
+    Important b/c this behavior changes slightly when running as frozen (.exe)
+    """
+
+    # main assets folder name
+    assets = "assets"
+
+    # subfolder names
+    icons = "icons"
+    fonts = "fonts"
+    templates = "templates"
+
+    # define relative paths
+    src_folder = os.path.dirname(os.path.abspath(__file__))
+    assets_folder = os.path.join(src_folder, assets)
+    app_icon = os.path.join(assets_folder, icons, "app.ico")
+    file_icon = os.path.join(assets_folder, icons, "file.ico")
+    font_file = os.path.join(assets_folder, fonts, "OpenSans-Regular.ttf")
+
+    cmd_list_template = os.path.join(
+        assets_folder, templates, "CommandList_TEMPLATE.txt"
+    )
+    out_template = os.path.join(assets_folder, templates, "OUT_TEMPLATE.out")
+
+    # this is just a  test file, delete me later
+    data_file = os.path.join(assets_folder, "data.csv")
+
+    if __name__ == "__main__":
+        print(file_browse())
